b4695d57d5a8b0bdcd688bb8fd969a57115ec6a7
Christian <22412104+lostXcode@users.noreply.github.com>
2022-03-31 14:41:28 +0200

    Deleted duplicate DeusExMachina folders.
    Deleted replays.

--- agent_code/DeusExMachina/callbacks.py
+++ agent_code/DeusExMachina/callbacks.py
@@ -1,15 +1,13 @@
 import os
-import datetime
 import glob
 import json
+import random
+
 import numpy as np
 
 from .features import state_dict_to_feature_str, store_unrecoverable_infos_helper
-from .parameter_decay import Explorer, PiggyCarry
 
-from .features import DeusExMachinaFeatures
-
-ALGORITHM = 'q-learning'
+ALGORITHM = 'q-forest'
 ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
 
 
@@ -25,77 +23,24 @@ def setup(self):
     self.ACTIONS = ACTIONS
     self.actions = range(len(ACTIONS))
 
-    with open("q-learning-params.json") as params_file:
-        self.logger.info("Loading q-learning parameters from json file")
+    self.q_tables = []
 
-        params = json.load(params_file)
-
-        self.gamma = params["reward_decay"]
-        self.explorer_type = params["explorer"]
-        self.exp_param = params["exp_param"]
-        self.exp_name = params["piggy_name"]
-
-        self.upd_type = params["update_type"]
-        self.upd_param = params["update_param"]
-
-        self.learning_type = params["learning_type"]
-        self.learning_param = params["learning_param"]
-        self.save_n_rounds = params["save_n_rounds"]
-        self.train_fast = params["train_fast"]
-
-        self.event_reward_set = params["event_reward_set"]
-        self.dyn_rewards = params["dyn_rewards"]
-
-        self.feature = params["feature"]
-        self.q_table_id = params["q_table_id"]
-    self.q_table = {}
-    self.n_table = {}
-    if self.explorer_type != "piggyback":
-        self.explorer = Explorer(self.explorer_type, self.exp_param)
-    else:
-        self.piggy = PiggyCarry(self.logger, self.exp_name, self.exp_param)
 
     # Check for q/n_table and load q_table from json
     q_table_filenames = glob.glob('q_table*.json')
-    self.q_table_filename = ''
-    self.n_table_filename = ''
-    last_q_table_dict = None
     for file in q_table_filenames:
         if not os.stat(file).st_size == 0:
             with open(file, "r") as q_table_file:
-                last_q_table_dict = json.load(q_table_file)
-                if "meta" in last_q_table_dict:
-                    if last_q_table_dict["meta"]["algorithm"] == ALGORITHM and \
-                            last_q_table_dict["meta"]["feature"] == self.feature and \
-                            last_q_table_dict["meta"]["q_table_id"] == self.q_table_id:
-                        self.q_table_filename = file
+                q_table_temp = {}
+                q_table_temp = json.load(q_table_file)
+                del q_table_temp["meta"]
+                self.q_tables.append(q_table_temp)
 
-                        # Check n_table
-                        n_table_filename_try = self.q_table_filename.replace("q", "n", 1)
-                        with open(n_table_filename_try, "r") as n_table_file:
-                            last_n_table_dict = json.load(n_table_file)
-                            if "meta" in last_n_table_dict:
-                                if last_n_table_dict["meta"]["algorithm"] == ALGORITHM and \
-                                        last_n_table_dict["meta"]["feature"] == self.feature and \
-                                        last_n_table_dict["meta"]["q_table_id"] == self.q_table_id:
-                                    self.n_table_filename = n_table_filename_try
-                                    break
-                                else:
-                                    self.logger.warn(f"q_table {self.q_table_filename} found without corresponding n_table.")
 
-    if not (self.q_table_filename == '' or self.n_table_filename == ''):
-        self.logger.info(f"Loading q_table from {self.q_table_filename} and n_table from {self.n_table_filename}")
-        del last_q_table_dict["meta"]
-        del last_n_table_dict["meta"]
-        self.q_table = last_q_table_dict
-        self.n_table = last_n_table_dict
-    else:
-        if not self.train:
-            self.logger.warn("Not training and no q_table found")
-        self.logger.info(f"No q_table*.json <-> n_table*.json pair found, for feature {self.feature} and id {self.q_table_id}. Empty one is used")
-        date_time_str = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        self.q_table_filename = f'q_table_{date_time_str}.json'
-        self.n_table_filename = f'n_table_{date_time_str}.json'
+    # Action layout for symmetry transformations
+    self.action_layout = np.array([[6, self.ACTIONS.index("UP"), 6],
+                                   [self.ACTIONS.index("LEFT"), 6, self.ACTIONS.index("RIGHT")],
+                                   [6, self.ACTIONS.index("DOWN"), 6]], dtype=np.int8)
 
     # Long/Short Term Memory
     self.prev_game_state_str = None
@@ -106,13 +51,6 @@ def setup(self):
     self.killed_opponents_scores = {}
     self.own_bomb_old = None
     self.own_bomb_new = None
-    self.total_rewards = 0
-
-    # Action layout for symmetry transformations
-    self.action_layout = np.array([[6, self.ACTIONS.index("UP"), 6],
-                                   [self.ACTIONS.index("LEFT"), 6, self.ACTIONS.index("RIGHT")],
-                                   [6, self.ACTIONS.index("DOWN"), 6]], dtype=np.int8)
-
 
 
 def act(self, game_state: dict) -> str:
@@ -124,7 +62,6 @@ def act(self, game_state: dict) -> str:
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
-
     # Reset at game start:
     if game_state["step"] == 1:
         self.prev_game_state_str = None
@@ -137,142 +74,117 @@ def act(self, game_state: dict) -> str:
         self.own_bomb_new = None
 
     # Store unrecoverable game state information
-    if self.feature in ["PreviousWinner", "DeusExMachinaFeatures"]:
-        if self.prev_game_state is not None:
-            self.remaining_coins_old = self.remaining_coins_new
-            self.own_bomb_old = self.own_bomb_new
+    if self.prev_game_state is not None:
+        self.remaining_coins_old = self.remaining_coins_new
+        self.own_bomb_old = self.own_bomb_new
 
-            coin_diff, killed_opponents_w_scores = store_unrecoverable_infos_helper(self.prev_game_state, game_state)
+        coin_diff, killed_opponents_w_scores = store_unrecoverable_infos_helper(self.prev_game_state, game_state)
 
-            self.remaining_coins_new -= coin_diff
-            self.killed_opponents_scores.update(killed_opponents_w_scores)
-            if self.own_bomb_old is None:
-                if game_state["self"][3] in [bomb[0] for bomb in game_state["bombs"]]:
-                    self.own_bomb_new = game_state["self"][3]
-            else:
-                if self.own_bomb_old not in [bomb[0] for bomb in game_state["bombs"]]:
-                    self.own_bomb_new = None
-
-        self.prev_game_state = game_state
-        game_state["remaining_coins"] = self.remaining_coins_new
-        game_state["own_bomb"] = self.own_bomb_new
-
-
-    if self.next_game_state_str is not None:
-        state_str = self.next_game_state_str
-    else:
-        state_str = state_dict_to_feature_str(game_state, self.feature)
-
-    # Symmetry check
-    transform = None
-    if type(state_str) is not str:
-        if self.feature not in ["RollingWindow", "DeusExMachinaFeatures"]:
-            self.logger.warn("Non-single-state-string only implemented for RollingWindow yet.")
-        state_idx = check_state_exist_w_sym(self, state_str[0])
-        if state_idx is None or state_str[1][0] is None:
-            # Just use the first entry w/o transform
-            state_str = state_str[0][0]
+        self.remaining_coins_new -= coin_diff
+        self.killed_opponents_scores.update(killed_opponents_w_scores)
+        if self.own_bomb_old is None:
+            if game_state["self"][3] in [bomb[0] for bomb in game_state["bombs"]]:
+                self.own_bomb_new = game_state["self"][3]
         else:
-            # Use transformed
-            transform = state_str[1][state_idx]
-            state_str = state_str[0][state_idx]
+            if self.own_bomb_old not in [bomb[0] for bomb in game_state["bombs"]]:
+                self.own_bomb_new = None
 
-    check_state_exist_and_add(self, state_str)
+    self.prev_game_state = game_state
+    game_state["remaining_coins"] = self.remaining_coins_new
+    game_state["own_bomb"] = self.own_bomb_new
 
-    # action selection
-    if self.explorer_type != "piggyback":
-        action = self.explorer.explore(self.actions, self.q_table[state_str], self.n_table[state_str])
+
+    sym_state_strs = state_dict_to_feature_str(game_state, "DeusExMachinaFeatures")
+    action_votes = [0] * len(self.actions)
+
+    # Let q trees give their votes
+    for q_table in self.q_tables:
+        # Check if any equivalent state is in this q table
+        index = check_state_exist_w_sym(sym_state_strs[0], q_table)
+        if index is not None: # found
+            existing_state_str = sym_state_strs[0][index]
+            transform = sym_state_strs[1][index]
+
+            # Get actions of maximal q value
+            max_q_actions = [idx for idx, val in enumerate(q_table[existing_state_str]) if val == max(q_table[existing_state_str])]
+
+            # transform actions back
+            for idx in range(len(max_q_actions)):
+                if not (max_q_actions[idx] >= 4):  # wait or bomb
+                    transformed_action_layout = self.action_layout
+                    if transform[1]:
+                        transformed_action_layout = transformed_action_layout.T
+                    transformed_action_layout = np.rot90(transformed_action_layout, k=(-1) * transform[0])
+                    max_q_actions[idx] = action_layout_to_action(self, transformed_action_layout, max_q_actions[idx])
+
+            # add count of actions
+            for actions in max_q_actions:
+                action_votes[actions] += 1
+
+        else: # not found
+            pass
+
+
+    # Calculate valid actions
+    # Gather information about the game state
+    arena = game_state['field']
+    player_pos = game_state['self'][3]
+    bomb_pos = [bomb[0] for bomb in game_state['bombs']]
+    enemy_pos = [enemy[3] for enemy in game_state['others']]
+
+    x, y = player_pos[0], player_pos[1]
+    neighbors = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+    valid_places, valid_actions, valid_movement_actions = [], [], []
+    for neighbor in neighbors:
+        if (arena[neighbor] == 0) and (neighbor not in bomb_pos) and (neighbor not in enemy_pos):
+            valid_places.append(neighbor)
+    if (x - 1, y) in valid_places:
+        valid_actions.append(ACTIONS.index('LEFT'))
+        valid_movement_actions.append(ACTIONS.index('LEFT'))
+    if (x + 1, y) in valid_places:
+        valid_actions.append(ACTIONS.index('RIGHT'))
+        valid_movement_actions.append(ACTIONS.index('RIGHT'))
+    if (x, y - 1) in valid_places:
+        valid_actions.append(ACTIONS.index('UP'))
+        valid_movement_actions.append(ACTIONS.index('UP'))
+    if (x, y + 1) in valid_places:
+        valid_actions.append(ACTIONS.index('DOWN'))
+        valid_movement_actions.append(ACTIONS.index('DOWN'))
+    valid_actions.append(ACTIONS.index('WAIT'))
+    if game_state['step'] != 1:
+        valid_actions.append(ACTIONS.index('BOMB'))
+
+
+    # Compute most voted valid action
+    #print(action_votes)
+
+    popular_actions = [idx for idx, val in enumerate(action_votes) if val == max(action_votes)]
+    valid_popular_actions = []
+    for action in popular_actions:
+        if action in valid_actions:
+            valid_popular_actions.append(action)
+
+
+    # if sum(action_votes) == 0:
+    #     print("New state encountered.")
+
+    if len(valid_popular_actions) > 0 and sum(action_votes) > 0:
+        action = random.choice(valid_popular_actions)
+    elif len(valid_movement_actions) > 0:
+        action = random.choice(valid_movement_actions)
     else:
-        action = ACTIONS.index(self.piggy.carry(game_state))
+        action = ACTIONS.index('WAIT')
 
     if time_for_bed(game_state, self):
         action = ACTIONS.index("BOMB")
 
-    # Transform action
-    if self.explorer_type != "piggyback" and transform is not None:
-        if not (action >= 4):  # wait or bomb
-            transformed_action_layout = self.action_layout
-            if transform[1]:
-                transformed_action_layout = transformed_action_layout.T
-            transformed_action_layout = np.rot90(transformed_action_layout, k=(-1) * transform[0])
-            action = action_layout_to_action(self, transformed_action_layout, action)
-
-    if not self.train_fast:
-        print("In act()")
-        DeusExMachinaFeatures(game_state).print_me()
-        qs = self.q_table[state_str]
-        qviz = np.array([[np.NAN, qs[0], np.NAN],
-                         [qs[3], qs[4], qs[1]],
-                         [np.NAN, qs[2], qs[5]]])
-        if transform is not None:
-            if transform[1]:
-                qviz = qviz.T
-            qviz = np.rot90(qviz, k=(-1) * transform[0])
-        print(qviz)
-        print("Action took: "+ACTIONS[action])
-
-    # Save state string and transform in short term memory
-    self.prev_game_state_str = ([state_str], [transform])
-
     return ACTIONS[action]
 
 
-def time_for_bed(game_state, self):
-    self_score = game_state["self"][1]
-    remaining_points_possible = self.remaining_coins_new + len(game_state["others"]) * 5
-    max_opponent_scores_possible = {}
-
-    for opp in game_state["others"]:
-        max_opponent_scores_possible[opp[0]] = opp[1] + remaining_points_possible - 5
-
-    killed_opponents_winning = len({opponent:score for (opponent,score) in self.killed_opponents_scores.items() if score >= self_score})
-    living_opponents_can_win = len({opponent:score for (opponent,score) in max_opponent_scores_possible.items() if score >= self_score})
-
-    if self_score >= 10:
-        return True
-    elif not killed_opponents_winning and not living_opponents_can_win:
-        return True
-    elif remaining_points_possible == 0: # killed opponents are winning, but no more points possible
-        return True
-    else:
-        return False
-
-
-def check_state_exist_and_add(self, state_str):
-    if state_str not in self.q_table:
-        # append new state to tables
-        if self.feature in ["DeusExMachinaFeatures"] and False:
-            self.q_table[state_str] = [8, 8, 8, 8, 7.9, 7.9]
-            for i in range(4):
-                if state_str[i] == "1" :
-                    self.q_table[state_str][i] = 10
-                elif state_str[i] == "2":
-                    self.q_table[state_str][i] = 0
-            if state_str[5] in ["0", "1", "2"]:
-                if state_str[:4].count("2") == 2 and ((state_str[:4].count("22") == 1) or (state_str[0] == "2" and state_str[3] == "2")):
-                    self.q_table[state_str][5] = -10
-                else:
-                    if state_str[4] == "0":
-                        pass
-                    elif state_str[4] == "1":
-                        self.q_table[state_str][5] = 10
-                    elif state_str[4] == "2":
-                        self.q_table[state_str][5] = 15
-                    elif state_str[4] == "3":
-                        self.q_table[state_str][5] = 20
-                    elif state_str[4] == "4":
-                        self.q_table[state_str][4] = -10
-                        self.q_table[state_str][5] = -10
-        else:
-            self.q_table[state_str] = [0] * len(self.actions)
-
-        self.n_table[state_str] = [1] * len(self.actions)
-
-
-def check_state_exist_w_sym(self, state_str_list: list[str]):
+def check_state_exist_w_sym(state_str_list: list[str], q_table: dict):
     idx = None
     for i, state_str in enumerate(state_str_list):
-        if state_str in self.q_table:
+        if state_str in q_table:
             idx = i
             break
     return idx
@@ -295,3 +207,23 @@ def action_layout_to_action(self, action_layout, action):
         act = None
 
     return act
+
+def time_for_bed(game_state, self):
+    self_score = game_state["self"][1]
+    remaining_points_possible = self.remaining_coins_new + len(game_state["others"]) * 5
+    max_opponent_scores_possible = {}
+
+    for opp in game_state["others"]:
+        max_opponent_scores_possible[opp[0]] = opp[1] + remaining_points_possible - 5
+
+    killed_opponents_winning = len({opponent:score for (opponent,score) in self.killed_opponents_scores.items() if score >= self_score})
+    living_opponents_can_win = len({opponent:score for (opponent,score) in max_opponent_scores_possible.items() if score >= self_score})
+
+    if self_score >= 10:
+        return True
+    elif not killed_opponents_winning and not living_opponents_can_win:
+        return True
+    elif remaining_points_possible == 0: # killed opponents are winning, but no more points possible
+        return True
+    else:
+        return False
